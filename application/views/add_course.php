10583b89814389a4d248f76bf6b0828a4ebee409
anji <anji.naga1@gmail.com>
2019-11-21 11:11:05 +0530

    add course added

--- application/views/add_course.php
+++ application/views/add_course.php
@@ -1,18 +1,20 @@
 <?php include('common/header.php'); ?>
+ <?php include('common/navbar.php') ?>
 <?php include('common/sidebar.php') ?>
 
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
+    
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Add User</h1>
+            <h1>Add Course</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Add User</li>
+              <li class="breadcrumb-item active">Add Course</li>
             </ol>
           </div>
         </div>
